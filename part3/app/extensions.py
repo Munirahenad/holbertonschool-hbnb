e86253977ac1fd73e1f01d24c7cb58ba6ccacdf0
munirahenad <muneraenad@hotmail.com>
2026-03-02 09:56:19 +0000

    Add extensions.py

---
+++ part3/app/extensions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Application extensions"""
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+jwt = JWTManager()
